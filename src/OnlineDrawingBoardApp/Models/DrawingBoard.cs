34e1c4b3379ac32b4972bb598dafb5f5e51e333a
Aleksei Sivodedov <sivoded@gmail.com>
2024-02-15 11:44:05 +0400

    feat: add new blazor app; use svgeditor nuget for drawing

---
+++ src/OnlineDrawingBoardApp/Models/DrawingBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineDrawingBoardApp.Models
+{
+    public class DrawingBoard
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
